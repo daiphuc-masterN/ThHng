266f1bd07cdba42fdaea057c35827792f5fc95c4
daiphuc-masterN <yountiger201@gmail.com>
2024-09-09 23:56:20 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,19 @@
 const CONFIG = {
     titleWeb: "Gửi Embe iu của anh",
     introTitle: 'Thúy Hằng iu ơiii',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introDesc: `Xin lõi vì đã làm em giận
+    Giận thì giận
+    Mà mình yêu thì yêu nhé embe`,
     btnIntro: '^^HiHi^^',
     title: 'Anh Yêu Em!!!! Làm bạn gái anh nhá embe iu? 🥰',
     desc: 'Thoát web là chấp nhận yêu tớ rồi nhé!!!',
     btnYes: 'Thích lắmmmm <33',
-    btnNo: 'Dạaaa, chịuuuu :3',
+    btnNo: 'Hong :3',
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
+    btnReply: 'Bấm iu anh <3 để thoát <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay, mình qua đón đi chơi nha.',
+    messDesc: 'Hẹn em nhìu ngày nhá',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://www.facebook.com/nguyendaiphuc2012/'
 }
